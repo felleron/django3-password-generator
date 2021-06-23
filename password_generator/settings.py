78ffe9435eceda10a62010a99a6149d1d7297dd4
Felleron <hypermori@gmail.com>
2021-06-23 22:17:13 +0430

    i made some tweaks

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&*f6taz#u!zi)#$eh775msnf#nn#4dz_0&whnu6ja^2+f38^o&'
+SECRET_KEY = 'django-insecure-&*f6taz#u!zi)#$eh775msdadwqewqadssnf#nn#4dz_0&whnu6ja^2+f38^o&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
